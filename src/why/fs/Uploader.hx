1f7f041c9ca211cd693b5a8f5fee582fa784ba3a
Kevin Leung <kevinresol@gmail.com>
2020-12-12 19:09:48 +0800

    unbloat

--- src/why/fs/Uploader.hx
+++ src/why/fs/Uploader.hx
@@ -7,20 +7,17 @@ import tink.io.Source;
 using tink.CoreApi;
 
 class Uploader {
-	public static function upload(request:Promise<OutgoingRequestHeader>, body:IdealSource) {
+	public static function upload(header:OutgoingRequestHeader, body:IdealSource) {
 		return Progress.make((progress, finish) -> {
-			request
-				.next(header -> {
-					fetch(header.url, {
-						method: header.method,
-						headers: [for (f in header) f],
-						body: body,
-						handlers: {
-							upload: v -> progress(v.value, v.total),
-						}
-					})
-						.all();
-				})
+			fetch(header.url, {
+				method: header.method,
+				headers: [for (f in header) f],
+				body: body,
+				handlers: {
+					upload: v -> progress(v.value, v.total),
+				}
+			})
+				.all()
 				.handle(finish);
 		});
 	}
